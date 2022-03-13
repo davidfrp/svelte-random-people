fe9e47ac4d38f9957ce6677300cbd5a1fc2ea7b6
davidfrp <davidfrp@outlook.dk>
2022-03-13 16:31:00 +0100

    Created a rudimentary table component.

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,28 @@
 <script>
-	export let name;
+	import PeopleTable from './components/PeopleTable.svelte';
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Hi Heather!</h1>
+	<PeopleTable tableData={[
+		{
+			name: 'Heather',
+			age: '24',
+			occupation: 'Student',
+			location: 'Bristol, England'
+		}
+	]} />
 </main>
 
 <style>
 	main {
-		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
 	}
-
+	
 	h1 {
+		text-align: center;
 		color: #ff3e00;
 		text-transform: uppercase;
 		font-size: 4em;
---
+++ src/components/PeopleTable.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let tableData = [];
+</script>
+
+<table>
+    <tr>
+        {#each Object.keys(tableData[0]) as row}
+            <td>{row}</td>
+        {/each}
+    </tr>
+    {#each tableData as rowData}
+        <tr>
+            {#each Object.values(rowData) as cellData}
+                <td>{cellData}</td>
+            {/each}
+        </tr>
+    {/each}
+</table>
